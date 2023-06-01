80872def3f7475bff0f1b4a5bf70be0f4245ca17
Jerrier Billian <1529146935@qq.com>
2023-06-01 17:11:19 +0800

    update

--- model/community.go
+++ model/community.go
@@ -17,14 +17,33 @@ func (c Community) TableName() string {
 }
 
 func CreateCommunity(c Community) (int, string) {
+	tx := util.DB.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
 	if len(c.Name) == 0 {
-		return -1, "群名称太短"
+		return -1, "群名称不能为空"
 	}
 	if c.OwnerId == 0 {
 		return -1, "请先登录"
 	}
 	if err := util.DB.Create(&c).Error; err != nil {
+		tx.Rollback()
 		return -1, "建群失败"
+	}
+
+	co := Contact{
+		OwnerId:  c.OwnerId,
+		TargetId: c.ID,
+		Type:     2,
+	}
+
+	if err := util.DB.Create(&co).Error; err != nil {
+		tx.Rollback()
+		return -1, "添加群聊关系失败"
 	}
 	return 1, "建群成功"
 }
